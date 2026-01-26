3d288ffd1d811a062c4ad1ea0c90e9f1b2942d20
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-01-26 15:10:30 +0530

    status code and database

--- Day-5/src/app.js
+++ Day-5/src/app.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const notes =[];
-app.get("/notes",(req,res)=>{
-    res.send(notes);
-});
-
+const notes = [];
 app.post("/notes",(req,res)=>{
-    console.log(req.body);
-   notes.push(req.body);
-   console.log(notes);
-   res.send("notes created successfully");
+    notes.push(req.body);
+    res.status(201).json({
+        message:"Notes created successfully"
+    })
+});
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        notes:notes
+    })
 });
 app.delete("/notes/:index",(req,res)=>{
     delete notes[req.params.index];
-    res.send("note deleted successfully");
-    console.log(notes);
-})
-
+    res.status(204).json({            // with the code 204 the postman will not show any data 
+        message:"notes deleted successfully"
+    });
+});
 app.patch("/notes/:index",(req,res)=>{
-     notes[req.params.index].description = req.body.description;
-    res.send("Data updated successfully");
-    console.log(notes);
+    notes[req.params.index].description = req.body.description;
+    res.status(200).json({
+        message:"Description is updated"
+    })
 })
 module.exports = app;
